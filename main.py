99175582c71f510b2196842cbe24a22ce6277b1c
JohannesK99 <johnny.klauer@gmail.com>
2025-01-11 18:22:57 +0100

    webapp

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
-import classifier
+from flask import Flask, render_template, request, url_for, redirect
 
-def main():
-    #name des classifiers und spalte, die vorhergesagt werden soll
-    #Valide Classifier: 'rf', 'svm', 'knn', 'naiveBayes', 'neuralNetwork'
-    #Valide Vorhersagespalten: 'Text zum Fertigungshilfsmittel', 'Stichprobenverfahren', 'Lenkungsmethode', 'Merkmalsgewichtung'
-    results = classifier.classifyData('svm', 'Text zum Fertigungshilfsmittel')
-    print(results)
-    return
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
+
+@app.route('/html')
+def html():
+    return render_template('index.html')
+
+'''
+@app.route('/auswertungen')
+def auswertungen():
+    return render_template('auswertungen.html')
+'''
+
+
+@app.route('/auswertungen', methods=['GET', 'POST'])
+def index_func():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('auswertungen'))
+    # show the form, it wasn't submitted
+    return render_template('auswertungen.html')
+
+'''
+@app.route('/monitoring', methods=['GET', 'POST'])
+def index_func():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('monitoring'))
+    # show the form, it wasn't submitted
+    return render_template('monitoring.html')
+'''
 
 if __name__ == '__main__':
-  main()
+    app.run(debug=True)
