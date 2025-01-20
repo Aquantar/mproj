2072d8e9a9f51463c2cf4c432b8bdae7f658a412
Aquantar <serdar.isik@outlook.de>
2025-01-20 19:49:03 +0100

    update

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,26 @@
-from app import app
+from app import app, results
 from flask import Flask, render_template, request, url_for, redirect
 from app.functions import *
+from io import BytesIO
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def html():
+    global inputFile
+    if request.method == 'POST':
+        print("success. starting prediction")
+        file = request.files['input']
+        trainData = pd.read_excel(file)
+        predData = pd.read_excel('data//testdata.xlsx')
+        modelType = 'rf'
+        results = createPrediction(trainData, predData, modelType)
+        return redirect(url_for('index_func'))
     return render_template('index.html')
 
 @app.route('/auswertungen', methods=['GET', 'POST'])
 def index_func():
-    #DAS IST FALSCH; NUR ZUM TESTEN OB ALLES FUNKTIONIERT, bis print(results)
-    
-    trainData = pd.read_excel('data//traindata.xlsx')
-    predData = pd.read_excel('data//testdata.xlsx')
-    modelType = 'rf'
-    results = createPrediction(trainData, predData, modelType)
     print(results)
-    
-    if request.method == 'POST':
-        # do stuff when the form is submitted
-        # redirect to end the POST handling
-        # the redirect can be to the same route or somewhere else
 
-        #input-datei wird hier irgendwie hochgeladen
-        #createPrediction funktion wird mit dieser input datei durchgeführt
-        #am ende kommt die results matrix raus, damit kann man machen was man möchte
-        
-        """
-        trainData = pd.read_excel('data//traindata.xlsx')
-        predData = pd.read_excel('data//testdata.xlsx')
-        modelType = 'rf'
-        results = modelTrainer.createPrediction(trainData, predData, modelType)
-        """
-        return redirect(url_for('auswertungen'))
-    # show the form, it wasn't submitted
     return render_template('auswertungen.html')
 
 @app.route('/monitoring', methods=['GET', 'POST'])
