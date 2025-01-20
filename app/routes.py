df1bb8c24db1a290c42d7a068197d102f07e0811
Aquantar <serdar.isik@outlook.de>
2025-01-20 20:53:52 +0100

    update

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,11 @@ from app.functions import *
 from io import BytesIO
 
 optionen = ["Option 1", "Option 2", "Option 3", "Option 4"]
+predictionDummy = {'Fertigungshilfsmittel': ['MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'FORM- UND LAGEMESSGERÄT', 'MESSUHR', 'MESSUHR', 'Konturenmessgerät', 'FORM- UND LAGEMESSGERÄT', 'Konturenmessgerät', 'Rauhigkeitsmessgerät', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR', 'MESSUHR'],
+                   'Stichprobenverfahren': ['1/1', '2/30', '2/30', '1/1', '1/1', '2/30', '2/30', '2/30', '2/30', '1/222', '1/1', '1/444', '2/30', '1/1', '1/444', '1/444', '1/444', '1/222', '1/120', '2/30', '2/30', '1/120', '1/120', '2/30', '2/30', '2/30', '2/30', '1/222', '1/1'],
+                   'Lenkungsmethode': [0.0, 0.0, 0.0, 33.0, 33.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 33.0, 0.0, 0.0, 0.0, 20.0, 20.0, 20.0, 33.0, 33.0, 20.0, 20.0, 20.0, 20.0, 20.0, 20.0],
+                   'Merkmalsgewichtung': [0, 0, 0, 'HM', 'HM', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'HM', 'HM', 0, 0, 0, 0, 0, 0]
+                  }
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -12,13 +17,16 @@ def html():
     if request.method == 'POST':
         print("success. starting prediction")
         file = request.files['input']
+        
         trainData = pd.read_excel('data//traindata.xlsx')
         predData = pd.read_excel(file)
-        #modelType = 'rf'
-        #results = createPrediction(trainData, predData, modelType)
+        modelType = 'rf'
+        #results = createPrediction(trainData, predData, modelType)         IST AUSKOMMENTIERT WEIL ES AKTUELL ZU LANGE DAUERT
+        results = predictionDummy                                           #FÜRS ERSTE EINFACH DIESE RESULTS BENUTZEN, SIND DIE DUMMY WERTE VON OBEN
+
         return redirect(url_for('index_func'))
     return render_template('index.html')
-#
+
 @app.route('/auswertungen', methods=['GET', 'POST'])
 def index_func():
     #if request.method == 'POST':
